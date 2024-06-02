bd4815fad732de55ead342922f2582c9915a059d
tsai1247 <jeff29cc91079@gmail.com>
2024-06-02 23:09:21 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ad Devourer
+Ad Devourer likes to eat advertisement.  He is hungry.
---
+++ main.js
@@ -0,0 +1,71 @@
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+    if (mutation.type === 'childList') {
+        replacement();
+    }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    // 配置 observer 觀察目標節點及其子節點
+    var config = { childList: true, subtree: true };
+
+    // 監聽整個文檔的 DOM 變化
+    observer.observe(document.body, config);
+});
+
+document.addEventListener("yt-navigate-start", (e) => {
+    const url = e?.detail?.url;
+    if (!url) return;
+    if (!url.startsWith("/shorts")) return;
+    const v = url.replace("shorts/", "").replace('/', '');
+    window.location = `https://www.youtube.com/watch?v=${v}`;
+});
+
+let i = -1;
+function replacement() {
+    
+    // 取得 url
+    const newURL = new URL(window.location);
+    const v = newURL.searchParams.get("v");
+
+    if (v === null) return;
+
+    const playerId = 'ytd-player';
+
+    const movie_player = document.getElementById('movie_player');
+
+    const oldElement = document.getElementById(playerId);
+
+    if (!oldElement || !movie_player) {
+        return;
+    }
+    
+    // 創建一個新的元素
+    const newElement = document.createElement('iframe');
+    newElement.id = playerId;
+    newElement.width = '100%';
+    newElement.height = '100%';
+    newElement.src = `https://www.youtube.com/embed/${v}`;
+    // newElement.frameBorder = "0";
+    newElement.allow = "accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    newElement.referrerpolicy = "strict-origin-when-cross-origin";
+    newElement.allowFullscreen = true;
+    newElement.autoplay = true;
+
+    i += 1;
+    // 替換舊元素
+    if (movie_player && i > 600) {
+        movie_player.remove();
+        oldElement.childNodes.forEach((node) => {
+            oldElement.removeChild(node);
+        });
+        
+        oldElement.parentNode.appendChild(newElement);
+        oldElement.remove();
+        // alert('replaced');
+        observer.disconnect();
+    }
+
+}
